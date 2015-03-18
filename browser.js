c384dc27128b7dfba0be08f7ac39ad86cb96323c
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-03-18 15:34:54 +0100

    Fix wrong root element >_<

--- browser.js
+++ browser.js
@@ -2,12 +2,13 @@
 
 import React from 'react';
 import Router from 'react-router';
+
 import routes from './routes';
 
 Router.run(routes, Router.HistoryLocation, (Handler) => {
   React.render(
     <Handler />,
-    document.getElementById('root')
+    document.getElementById('app')
   );
 });
 
