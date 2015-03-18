b9efdc9b17a2e88b7ba970b977f3f3dc789cc29d
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-03-18 18:52:11 +0100

    Slightly change client side test

--- components/Home.js
+++ components/Home.js
@@ -5,19 +5,19 @@ import React from 'react';
 var Home = React.createClass({
   getInitialState: function () {
     return {
-      message: null
+      message: 'Probably not'
     };
   },
-  handleClick: function () {
+  componentDidMount: function () {
     this.setState({
-      message: 'Event bindings work'
+      message: 'Yes'
     });
   },
   render: function () {
     return (
       <div className="container">
         <h3>Home</h3>
-        <button onClick={this.handleClick}>Test event bindings</button>
+        <p>Is JavaScript loaded?</p>
         <small>{this.state.message}</small>
       </div>
     );
